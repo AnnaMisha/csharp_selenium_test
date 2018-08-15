3145ca0c3fdf8ead76cc0f3cc27f201b3ff89477
AnnaMisha <42388961@users.noreply.github.com>
2018-08-14 20:17:51 -0600

    Project created

---
+++ csharp_selenium_test/csharp_selenium_test/FirstTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.ya.ru/";
+            driver.FindElement(By.Id("text")).SendKeys("webdriver");
+            driver.FindElement(
+                By.XPath("/html/body/table/tbody/tr[2]/td/form/div[2]")
+                ).Click();
+            Thread.Sleep(5000);
+            //wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ csharp_selenium_test/csharp_selenium_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csharp_selenium_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("csharp_selenium_test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9115eddf-1a3a-456f-90e2-c3d785bef990")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
